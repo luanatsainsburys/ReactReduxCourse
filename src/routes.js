a1bae00eb408be2a1917fa9d2e442983a5180d0d
luanatsainsburys <luan.au@sainsburys.co.uk>
2016-07-11 16:58:04 +0100

    Save work

---
+++ src/routes.js
@@ -0,0 +1,13 @@
+import React from 'react';
+import { Route, IndexRoute } from 'react-router';
+
+import App from './components/App';
+import HomePage from './components/HomePage';
+import AboutPage from './components/AboutPage';
+
+export default (
+  <Route path="/" component={App}>
+    <IndexRoute component={HomePage}/>
+    <Route path="about" component={AboutPage}/>
+  </Route>
+);
