a09821834284b5f55bdf4de72772c8fe1b13c810
Luan Au <Luan Au>
2016-07-15 22:57:57 +0100

    Show Jumbotron

--- src/components/home/HomePage.js
+++ src/components/home/HomePage.js
@@ -6,8 +6,8 @@ class HomePage extends React.Component {
     return (
       <div className="jumbotron">
         <h1>Pluralsight Admininistration</h1>
-        <p>React, Redu and React Router in ES6 for ultra-responsive web apps.</p>
-        <Link to="about" className="btn btn-primary btn-lg">Learn more</Link>Link>
+        <p>React, Redux and React Router in ES6 for ultra-responsive web apps.</p>
+        <Link to="about" className="btn btn-primary btn-lg">Learn more</Link>
       </div>
     );
   }
--- src/routes.js
+++ src/routes.js
@@ -2,8 +2,8 @@ import React from 'react';
 import { Route, IndexRoute } from 'react-router';
 
 import App from './components/App';
-import HomePage from './components/HomePage';
-import AboutPage from './components/AboutPage';
+import HomePage from './components/home/HomePage';
+import AboutPage from './components/about/AboutPage';
 
 export default (
   <Route path="/" component={App}>
