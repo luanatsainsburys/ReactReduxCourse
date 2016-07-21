901c5e94dd113e46abf3a059449820f253fb81eb
luanau <luanau@gmail.com>
2016-07-21 11:07:05 +0100

    Create CourseForm

--- src/components/course/CourseForm.js
+++ src/components/course/CourseForm.js
@@ -8,14 +8,14 @@ const CourseForm = ({course, allAuthors, onSave, onChange, loading, errors}) => 
       <h1>Manage Course</h1>
       <TextInput
       name="tittle"
-      lable="Title"
+      label="Title"
       value={course.title}
       onChange={onChange}
       error={errors.title}/>
 
       <SelectInput
         name="authorId"
-        lable="Author"
+        label="Author"
         value={course.authorId}
         defaultOption="Select Author"
         options={allAuthors}
@@ -24,14 +24,14 @@ const CourseForm = ({course, allAuthors, onSave, onChange, loading, errors}) => 
 
       <TextInput
         name="category"
-        lable="Category"
+        label="Category"
         value={course.category}
         onChange={onChange}
         error={errors.category}/>
 
       <TextInput
         name="length"
-        lable="Length"
+        label="Length"
         value={course.length}
         onChange={onChange}
         error={errors.length}/>
