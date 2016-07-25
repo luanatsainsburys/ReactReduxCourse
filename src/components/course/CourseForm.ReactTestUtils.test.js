1ad92dea475b0b492dc6261bc0a0e21bbeca23c4
luanau <luanau@gmail.com>
2016-07-25 12:44:44 +0100

    Add React testing

--- src/components/course/CourseForm.ReactTestUtils.test.js
+++ src/components/course/CourseForm.ReactTestUtils.test.js
@@ -3,9 +3,9 @@ import React from 'react';
 import TestUtils from 'react-addons-test-utils';
 import CourseForm from './CourseForm';
 
-function setup() {
+function setup(saving) {
   let props = {
-    course: {}, saving: false, errors: {},
+    course: {}, saving: saving, errors: {},
     onSave: () => {},
     onChange: () => {}
   };
@@ -28,4 +28,17 @@ describe('CourseForm via React Test Utils', () => {
     let [h1] = output.props.children;
     expect(h1.type).toBe('h1');
   });
+
+  it('save button is labelled "Save" when not saving', () => {
+    const {output} = setup(false);
+    const submitButton = output.props.children[5];
+    expect(submitButton.props.value).toBe('Save');
+  });
+
+  it('save button is labelled "Saving..." when saving is true', () => {
+    const {output} = setup(true);
+    const submitButton = output.props.children[5];
+    expect(submitButton.props.value).toBe('Saving...');
+  });
+
 });
